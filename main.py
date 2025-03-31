8c5ee01249fffe56fb69ac519b4dd9fe3f6fc283
Felipo Olegario <felipoolegario@hotmail.com>
2025-03-31 19:04:17 -0300

    refatoração: aprimorar o layout e o estilo da IU

--- main.py
+++ main.py
@@ -55,27 +55,78 @@ async def processar_imagens(page, files, progress_bar, status_text, progress_tex
     # Abrir a pasta automaticamente após a conclusão
     os.startfile(output_folder)
 
+
+
+
+
 def main(page: ft.Page):
     page.title = "Conversor HEIC para PNG"
-    page.window_width = 500
-    page.window_height = 400
+    page.bgcolor = '#9C6675'
+    page.window.width = 500
+    page.window.height = 460
+    page.window.resizable = False
+    page.window.maximizable = False
+    page.window.center()
     
-
+    container = ft.Container(
+        content=ft.Column(
+            [
+                ft.Text("Conversor HEIC para PNG", size=24, weight="bold", color="#F1D7C5"),
+            ],
+            alignment="center",
+            horizontal_alignment="center",
+        ),
+        width=500,
+        height=400,
+    )
 
     file_picker = ft.FilePicker()
-    progress_bar = ft.ProgressBar(width=400)
-    progress_bar.value = 0
-    progress_bar.visible = False
-    progress_text = ft.Text("0% concluído", size=14, color="gray")
-    progress_text.visible = False
-    progress_text.visible = False
-    status_text = ft.Text("Selecione imagens HEIC para converter", size=16)
+    progress_bar = ft.ProgressBar(width=400,color='#CA7077',bgcolor='#F0D9C6', border_radius=10, height=20, visible=False)
+    progress_text = ft.Text("0% concluído", size=14, color="#F1D7C5", visible=False)
+    status_text = ft.Text("Selecione imagens HEIC para converter", size=16, color="#F1D7C5")
+
+    estilo_bt=ft.ButtonStyle(
+    shape=ft.RoundedRectangleBorder(radius=2),
+    bgcolor={
+        ft.ControlState.DEFAULT: "#CA7077",
+        ft.ControlState.FOCUSED: "#5caee0",
+        ft.ControlState.PRESSED: "#764E5D",
+    },
+    color={
+        ft.ControlState.DEFAULT: "#F1D7C5",
+        ft.ControlState.PRESSED: ft.colors.WHITE60,
+    },
+    elevation={
+        ft.ControlState.DEFAULT: 3,
+        ft.ControlState.HOVERED: 5,
+    },
+    padding=15,
+    text_style={
+        ft.ControlState.DEFAULT: ft.TextStyle(
+            size=15,
+            # weight="w500"
+        )
+    },
+    
+)
+    
+    def clickar_botao(e):
+        progress_bar.value = 0
+        progress_text.value = "0% concluído"
+        progress_bar.visible = True
+        progress_text.visible = True
+        progress_bar.update()
+        progress_text.update()
+        file_picker.pick_files(allow_multiple=True)
+        progress_text.value = "Iniciando conversão..."
+        progress_text.update()
+
 
     async def selecionar_arquivos(e: ft.FilePickerResultEvent):
         if e.files:
             progress_bar.visible = True
             progress_text.visible = True
-            progress_bar.value = 0  # Resetar a barra antes de cada nova conversão
+            progress_bar.value = 0
             progress_text.value = "0% concluído"
             progress_bar.update()
             progress_text.update()
@@ -85,10 +136,17 @@ def main(page: ft.Page):
     
     file_picker.on_result = selecionar_arquivos
     
-    btn_escolher = ft.ElevatedButton("Escolher imagens", on_click=lambda _: file_picker.pick_files(allow_multiple=True))
-
-
+    btn_escolher = ft.ElevatedButton("Escolher imagens", 
+        style=estilo_bt,
+        icon="search",
+        icon_color="#F1D7C5",
+        width=345,
+        on_click=clickar_botao
+        )
     
-    page.add(btn_escolher, progress_bar, progress_text, status_text, file_picker)
+    container.content.controls.extend([progress_bar, progress_text, status_text ,btn_escolher, file_picker])
+    
+
+    page.add(container)
     
 ft.app(target=main)
